3f84465c3b960c2b608fc48cb8a1bd92a4620d02
Tamanna Swami <tswami@xpanxion.com>
2019-01-18 14:57:54 +0530

    retest item application user

--- WishListTests/UpdateModelRelationshipTests.cs
+++ WishListTests/UpdateModelRelationshipTests.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using WishList.Models;
 using Xunit;
 
@@ -15,15 +13,16 @@ namespace WishListTests
             var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "WishList" + Path.DirectorySeparatorChar + "Models" + Path.DirectorySeparatorChar + "Item.cs";
             Assert.True(File.Exists(filePath), @"`Item.cs` was not found in the `Models` folder, did you accidentally rename or remove it?");
 
-            var item = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+            var item = /*(from assembly in AppDomain.CurrentDomain.GetAssemblies()
                         from type in assembly.GetTypes()
                         where type.FullName == "WishList.Models.Item"
-                        select type).FirstOrDefault();
+                        select type).FirstOrDefault();*/
+                        typeof(WishList.Models.Item);
             Assert.True(item != null, "A `public` class `Item` was not found in the `WishList.Models` namespace, did you accidentally rename or remove it?");
 
             var userProperty = item.GetProperty("User");
             Assert.True(userProperty != null, "`Item` does not appear to contain a `public` `virtual` `ApplicationUser` property `User`");
-            Assert.True(userProperty.PropertyType == typeof(ApplicationUser),"`Item` contained a property `User` but it was not of type `ApplicationUser`");
+            Assert.True(userProperty.PropertyType == typeof(ApplicationUser), "`Item` contained a property `User` but it was not of type `ApplicationUser`");
             Assert.True(userProperty.GetMethod.IsVirtual, "`Item` contained a property `User` but it didn't use the `virtual` keyword.");
         }
 
@@ -33,10 +32,11 @@ namespace WishListTests
             var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "WishList" + Path.DirectorySeparatorChar + "Models" + Path.DirectorySeparatorChar + "ApplicationUser.cs";
             Assert.True(File.Exists(filePath), @"`ApplicationUser.cs` was not found in the `Models` folder, did you accidentally rename or remove it?");
 
-            var item = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+            var item = /*(from assembly in AppDomain.CurrentDomain.GetAssemblies()
                         from type in assembly.GetTypes()
                         where type.FullName == "WishList.Models.ApplicationUser"
-                        select type).FirstOrDefault();
+                        select type).FirstOrDefault();*/
+                        typeof(WishList.Models.ApplicationUser);
             Assert.True(item != null, "A `public` class `ApplicationUser` was not found in the `WishList.Models` namespace, did you accidentally rename or remove it?");
 
             var itemsProperty = item.GetProperty("Items");
