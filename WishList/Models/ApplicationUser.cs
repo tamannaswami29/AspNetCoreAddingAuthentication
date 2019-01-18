9a363dd293fcf594277a6c9251f884cebddd78c7
Tamanna Swami <tswami@xpanxion.com>
2019-01-18 14:46:13 +0530

    create relationship between item and application user

--- WishList/Models/ApplicationUser.cs
+++ WishList/Models/ApplicationUser.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
-
+using System.Collections.Generic;
 namespace WishList.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<Item> Items { get; set; }
     }
 }
